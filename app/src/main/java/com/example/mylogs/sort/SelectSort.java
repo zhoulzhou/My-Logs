09ac03bec3116aeb09c94c83747906cad4d96971
wangshu <wang.shu4@byd.com>
2022-02-16 13:52:14 +0800

    select sort

---
+++ app/src/main/java/com/example/mylogs/sort/SelectSort.java
@@ -0,0 +1,43 @@
+package com.example.mylogs.sort;
+
+public class SelectSort {
+    public static void sort(int[] array){
+        if(array == null || array.length == 0){
+            return;
+        }
+
+        int minIndex = 0;
+        for (int i=0; i<array.length-1; i++){
+            minIndex = i;
+            for (int j=i+1; j<array.length; j++){
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i){
+                swap(array, minIndex, i);
+            }
+        }
+
+        printArray(array);
+    }
+
+    private static void swap(int[] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static void printArray(int[] array){
+        for (int i=0; i<array.length; i++){
+            System.out.println(" array= " + array[i]);
+        }
+    }
+
+    public static void test(){
+        System.out.println("---- test  ---");
+        int[] array = {4,3,7,9,12,42,5,22,6,8,2,34,1};
+        sort(array);
+    }
+}
