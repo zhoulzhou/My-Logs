d6fe2ab1684ae0bd530e91f13b1f32f391b24863
wangshu <wang.shu4@byd.com>
2022-03-18 15:37:24 +0800

    View 11

--- app/src/main/java/com/example/mylogs/Doc/DocMain.java
+++ app/src/main/java/com/example/mylogs/Doc/DocMain.java
@@ -4,7 +4,8 @@ package com.example.mylogs.Doc;
  * View点击事件分发
  * https://www.jianshu.com/p/238d1b753e64
  *
- *
+ *View绘制流程
+ * https://www.jianshu.com/p/060b5f68da79
  *
  */
 public class DocMain {
