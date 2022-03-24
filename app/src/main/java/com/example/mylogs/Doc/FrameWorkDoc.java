224cd79b3c75fa6abae7d7ec8b2d83f78d50225c
keemw <sohokeem@outlook.com>
2022-03-24 14:23:51 +0800

    Activity启动流程 1

--- app/src/main/java/com/example/mylogs/Doc/FrameWorkDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/FrameWorkDoc.java
@@ -7,6 +7,13 @@ package com.example.mylogs.Doc;
  * https://juejin.cn/post/6844903959581163528
  *https://zhuanlan.zhihu.com/p/433901409
  *
+ * Launcher进程请求AMS
+ * AMS发送创建应用进程请求
+ * Zygote进程接受请求并孵化应用进程
+ * 应用进程启动ActivityThread
+ * 应用进程绑定到AMS
+ * AMS发送启动Activity的请求
+ * ActivityThread的Handler处理启动Activity的请求
  *
  */
 public class FrameWorkDoc {
