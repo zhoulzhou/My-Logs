6333ee3e87444a9638a81ae0f2979294a20f3223
keemw <sohokeem@outlook.com>
2022-03-24 16:49:03 +0800

    常用线程池及适用场景1

--- app/src/main/java/com/example/mylogs/Doc/ThreadDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/ThreadDoc.java
@@ -3,6 +3,8 @@ package com.example.mylogs.Doc;
 /**
  * 常用线程池及适用场景
  * https://blog.csdn.net/fajing_feiyue/article/details/102644223
+ * https://blog.csdn.net/qq_45076180/article/details/108316340
+ * https://tech.meituan.com/2020/04/02/java-pooling-pratice-in-meituan.html
  *
  */
 public class ThreadDoc {
