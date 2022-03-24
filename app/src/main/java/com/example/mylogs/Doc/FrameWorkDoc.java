95e95e390effd264acfd7876d7e97ded4bf12bb1
keemw <sohokeem@outlook.com>
2022-03-24 14:04:44 +0800

    Activity启动流程

--- app/src/main/java/com/example/mylogs/Doc/FrameWorkDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/FrameWorkDoc.java
@@ -4,6 +4,8 @@ package com.example.mylogs.Doc;
  * Activity启动流程
  * https://www.jianshu.com/p/53d63374619d   参数说明
  *
+ * https://juejin.cn/post/6844903959581163528
+ *https://zhuanlan.zhihu.com/p/433901409
  *
  *
  */
