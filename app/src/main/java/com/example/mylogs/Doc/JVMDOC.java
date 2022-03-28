7c531244e0902248e5a5bcfcbcb0c63ea072ae00
keemw <sohokeem@outlook.com>
2022-03-28 11:32:00 +0800

    JVM

---
+++ app/src/main/java/com/example/mylogs/Doc/JVMDOC.java
@@ -0,0 +1,13 @@
+package com.example.mylogs.Doc;
+
+/**
+ * JVM内存模型分析
+ * https://baijiahao.baidu.com/s?id=1722629637623477825&wfr=spider&for=pc
+ *
+ *
+ * JVM垃圾回收算法
+ * https://blog.csdn.net/aijiudu/article/details/72991993
+ *
+ */
+public class JVMDOC {
+}
