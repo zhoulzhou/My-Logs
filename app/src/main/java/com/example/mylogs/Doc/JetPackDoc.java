5ab2739c6be62e20d7814c7d4dcd6e0f91f2a893
keemw <sohokeem@outlook.com>
2022-04-01 15:35:55 +0800

    viewbinding

--- app/src/main/java/com/example/mylogs/Doc/JetPackDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/JetPackDoc.java
@@ -4,6 +4,9 @@ package com.example.mylogs.Doc;
  * LifeCycle
  *
  *
+ * ViewBinding
+ * https://blog.csdn.net/zjuter/article/details/110817123?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ETopBlog-1.topblog&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ETopBlog-1.topblog&utm_relevant_index=1
+ *
  */
 public class JetPackDoc {
 }
