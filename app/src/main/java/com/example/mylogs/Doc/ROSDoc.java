e7734be23e7fc43635eb28c636fe25629e49f189
keemw <sohokeem@outlook.com>
2022-04-01 11:27:12 +0800

    ros 1

--- app/src/main/java/com/example/mylogs/Doc/ROSDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/ROSDoc.java
@@ -5,6 +5,9 @@ package com.example.mylogs.Doc;
  * https://b23.tv/x8QYQe3
  *
  * https://cloud.tencent.com/developer/article/1820014
+ *
+ * https://developer.aliyun.com/mirror/?spm=a2c6h.25603864.0.0.7f684ccazkLbTA
+ * http://mirrors.aliyun.com/ubuntu-releases/20.04/
  */
 public class ROSDoc {
 }
