10be54bf7220e56a26816783f667195114e0c74e
keemw <sohokeem@outlook.com>
2022-04-06 15:39:14 +0800

    lc 11

--- app/src/main/java/com/example/mylogs/Doc/LeakDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/LeakDoc.java
@@ -4,6 +4,9 @@ package com.example.mylogs.Doc;
  * 内存优化
  * https://www.cnblogs.com/Android-Alvin/p/12596813.html
  *
+ * LeakCanary原理
+ * https://blog.csdn.net/huo108/article/details/83691285
+ *
  */
 public class LeakDoc {
 }
