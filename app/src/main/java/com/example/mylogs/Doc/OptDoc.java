2e14cc8343cbbe3d275ff7fb06624e88e47de3fd
keemw <sohokeem@outlook.com>
2022-04-13 16:01:18 +0800

    卡顿检测

---
+++ app/src/main/java/com/example/mylogs/Doc/OptDoc.java
@@ -0,0 +1,14 @@
+package com.example.mylogs.Doc;
+
+/**
+ * ANR
+ * ANR的日志在data/anr/traces.txt目录下
+ *
+ * Handler打印卡顿信息
+ * https://blog.csdn.net/u011578734/article/details/109582595
+ * https://blog.csdn.net/u011578734/article/details/106288970
+ * https://blog.csdn.net/weixin_29163857/article/details/117508707
+ *
+ */
+public class OptDoc {
+}
