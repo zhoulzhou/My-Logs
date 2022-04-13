cb0df7484ac99679b3b86344a7def73c7bca38a8
keemw <sohokeem@outlook.com>
2022-04-13 13:48:55 +0800

    handler

--- app/src/main/java/com/example/mylogs/Doc/AndroidAsyncDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/AndroidAsyncDoc.java
@@ -1,18 +1,6 @@
 package com.example.mylogs.Doc;
 
 /**
- *Handler原理分析
- * https://www.cnblogs.com/ivoo/p/10727286.html
- *https://blog.51cto.com/u_14347056/2398080
- *https://www.jianshu.com/p/194d5d4e877d
- *
- * EventFD epoll机制 ***
- * https://www.cnblogs.com/renhui/p/12875396.html
- * https://jishuin.proginn.com/p/763bfbd35a2d
- *
- * Handler postDelay
- * https://www.jianshu.com/p/44b322dfc040
- *
  *
  *AsyncTask原理
  * https://blog.csdn.net/wjinhhua/article/details/60578133
