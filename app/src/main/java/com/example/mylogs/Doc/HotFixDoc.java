f152923673fd42ff490190395bafd17263484b0a
keemw <sohokeem@outlook.com>
2022-04-15 10:07:28 +0800

    hotfix

--- app/src/main/java/com/example/mylogs/Doc/HotFixDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/HotFixDoc.java
@@ -2,7 +2,7 @@ package com.example.mylogs.Doc;
 
 /**
  * 热修复
- * https://baijiahao.baidu.com/s?id=1711347648018216183&wfr=spider&for=pc
+ * https://baijiahao.baidu.com/s?id=1712164667892298760&wfr=spider&for=pc
  *
  */
 public class HotFixDoc {
