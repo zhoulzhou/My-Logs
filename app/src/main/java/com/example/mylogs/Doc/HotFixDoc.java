955e06b0d391fe268abd4d32a1e9c87fb5c5852b
keemw <sohokeem@outlook.com>
2022-04-15 14:00:01 +0800

    hotfix 32

--- app/src/main/java/com/example/mylogs/Doc/HotFixDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/HotFixDoc.java
@@ -3,6 +3,8 @@ package com.example.mylogs.Doc;
 /**
  * 热修复
  * https://baijiahao.baidu.com/s?id=1712164667892298760&wfr=spider&for=pc
+ * https://blog.csdn.net/ljx1400052550/article/details/115515676?spm=1001.2101.3001.6650.7&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-7.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-7.pc_relevant_aa&utm_relevant_index=14
+ *
  * https://bthvi-leiqi.blog.csdn.net/article/details/80954639?spm=1001.2101.3001.6650.6&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-6.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-6.pc_relevant_aa&utm_relevant_index=13
  *
  */
