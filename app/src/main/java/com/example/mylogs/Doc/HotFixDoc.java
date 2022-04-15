cb981d1af5411f77b7f2591049aa82fb02be5156
keemw <sohokeem@outlook.com>
2022-04-15 15:20:35 +0800

    组件化

--- app/src/main/java/com/example/mylogs/Doc/HotFixDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/HotFixDoc.java
@@ -8,6 +8,12 @@ package com.example.mylogs.Doc;
  *
  * https://bthvi-leiqi.blog.csdn.net/article/details/80954639?spm=1001.2101.3001.6650.6&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-6.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-6.pc_relevant_aa&utm_relevant_index=13
  *
+ *插件化和热修复知识梳理
+ * https://www.jianshu.com/p/704cac3eb13d
+ *
+ * 组件化
+ * https://blog.csdn.net/weixin_43901866/article/details/110822240
+ *
  */
 public class HotFixDoc {
 }
