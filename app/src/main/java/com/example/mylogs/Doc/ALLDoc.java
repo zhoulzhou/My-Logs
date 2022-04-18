c0967ab753caa918f21bf4461d1c36b28542fb71
keemw <sohokeem@outlook.com>
2022-04-18 17:03:42 +0800

    data

--- app/src/main/java/com/example/mylogs/Doc/ALLDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/ALLDoc.java
@@ -1,8 +1,11 @@
 package com.example.mylogs.Doc;
 
 /**
- *
+ *面试知识集结
  * https://www.kancloud.cn/aslai/interview-guide/1126387
+ *
+ * 好的博客
+ * https://juejin.cn/user/3227821828236392/posts
  *
  */
 public class ALLDoc {
--- app/src/main/java/com/example/mylogs/Doc/ThirdLibDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/ThirdLibDoc.java
@@ -3,6 +3,10 @@ package com.example.mylogs.Doc;
 /**
  * EventBus
  * https://www.jianshu.com/p/56540e07416a
+ * https://juejin.cn/post/6844903944561377293
+ *
+ * ARoute
+ *
  *
  */
 public class ThirdLibDoc {
