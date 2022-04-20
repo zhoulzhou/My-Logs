ae0c5f4e75db81758aa13667d67658739d507313
keemw <sohokeem@outlook.com>
2022-04-20 10:20:51 +0800

    EVENTBUS

--- app/src/main/java/com/example/mylogs/Doc/ThirdLibDoc.java
+++ app/src/main/java/com/example/mylogs/Doc/ThirdLibDoc.java
@@ -2,6 +2,7 @@ package com.example.mylogs.Doc;
 
 /**
  * EventBus
+ * https://zhuanlan.zhihu.com/p/338474770
  * https://www.jianshu.com/p/56540e07416a
  * https://juejin.cn/post/6844903944561377293
  *
